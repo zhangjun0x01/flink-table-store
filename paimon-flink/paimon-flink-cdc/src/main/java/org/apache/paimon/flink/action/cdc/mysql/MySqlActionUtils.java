b9079a387587788d8cd87fdd63bd8939b0303580
zhangjun <zhangjunemail@126.com>
2023-12-05 10:54:26 +0800

    upgrade flink mysql cdc to 2.4.2

--- paimon-flink/paimon-flink-cdc/src/main/java/org/apache/paimon/flink/action/cdc/mysql/MySqlActionUtils.java
+++ paimon-flink/paimon-flink-cdc/src/main/java/org/apache/paimon/flink/action/cdc/mysql/MySqlActionUtils.java
@@ -33,8 +33,6 @@ import com.ververica.cdc.connectors.mysql.table.JdbcUrlUtils;
 import com.ververica.cdc.connectors.mysql.table.StartupOptions;
 import com.ververica.cdc.debezium.JsonDebeziumDeserializationSchema;
 import com.ververica.cdc.debezium.table.DebeziumOptions;
-import org.apache.flink.configuration.ConfigOption;
-import org.apache.flink.configuration.ConfigOptions;
 import org.apache.flink.configuration.Configuration;
 import org.apache.kafka.connect.json.JsonConverterConfig;
 import org.slf4j.Logger;
@@ -59,13 +57,6 @@ import static org.apache.paimon.utils.Preconditions.checkArgument;
 public class MySqlActionUtils {
 
     private static final Logger LOG = LoggerFactory.getLogger(MySqlActionUtils.class);
-
-    public static final ConfigOption<Boolean> SCAN_NEWLY_ADDED_TABLE_ENABLED =
-            ConfigOptions.key("scan.newly-added-table.enabled")
-                    .booleanType()
-                    .defaultValue(true)
-                    .withDescription(
-                            "Whether capture the scan the newly added tables or not, by default is true.");
 
     static Connection getConnection(Configuration mySqlConfig, boolean tinyint1NotBool)
             throws Exception {
@@ -218,13 +209,7 @@ public class MySqlActionUtils {
         JsonDebeziumDeserializationSchema schema =
                 new JsonDebeziumDeserializationSchema(true, customConverterConfigs);
 
-        boolean scanNewlyAddedTables = mySqlConfig.get(SCAN_NEWLY_ADDED_TABLE_ENABLED);
-
-        return sourceBuilder
-                .deserializer(schema)
-                .includeSchemaChanges(true)
-                .scanNewlyAddedTableEnabled(scanNewlyAddedTables)
-                .build();
+        return sourceBuilder.deserializer(schema).includeSchemaChanges(true).build();
     }
 
     public static void registerJdbcDriver() {
